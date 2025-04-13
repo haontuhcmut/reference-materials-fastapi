920150852171b01da98126edeadb95ffa3f818ef
haontuhcmut <nguyenminhhao1188@gmail.com>
2025-04-13 10:50:36 +0700

    handling inventory

--- app/transaction/schema.py
+++ app/transaction/schema.py
@@ -8,23 +8,27 @@ class TransactionType(Enum):
     EXPORT = "export"
 
 
-class ItemList(BaseModel):
-    item_type_id: UUID
-    quantity: float = Field(default=0, ge=0, le=99999)
-
 class CreateTransactionsModel(BaseModel):
     warehouse_id: UUID
-    item_type_id: list[ItemList]
+    item_type_id: UUID
     transaction_type: TransactionType
+    quantity: float = Field(default=0, ge=0, le=99999)
     description: str = Field(default=None, max_length=1024)
 
-# class CreateTransactionModel(BaseModel):
-#     warehouse_id: UUID
-#     item_type_id: UUID
-#     transaction_type: str = Field(default=None, max_length=64)
-#     quantity: float = Field(default=0, ge=0, le=99999)
-#     description: str = Field(default=None, max_length=1024)
-#
-#
-# class TransactionModel(CreateTransactionModel):
-#     id: UUID
+class TransactionModel(CreateTransactionsModel):
+    id: UUID
+
+class ImportStockModel(BaseModel):
+    item_type_id: UUID
+    warehouse_id: UUID
+    transaction_type: TransactionType.IMPORT
+    quantity: float = Field(default=0, ge=0, le=99999)
+    description: str | None = Field(default=None, max_length=1024)
+
+class ExportStockModel(BaseModel):
+    item_type_id: UUID
+    warehouse_id: UUID
+    transaction_type: TransactionType.EXPORT
+    quantity: float = Field(default=0, ge=0, le=99999)
+    description: str | None = Field(default=None, max_length=1024)
+
